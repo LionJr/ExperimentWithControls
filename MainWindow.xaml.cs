15fc2c1aeb9484a91a294c253c2d75fd51de3967
LionJr <arslan.enigma.man@gmail.com>
2023-07-12 22:51:14 +0500

    Now we have complited our project

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,9 +28,40 @@ namespace ExperimentWithControls
         {
             number.Text = numberTextBox.Text;
         }
-        private void numberTextBox_PreviewText(object sender, TextCompositionEventArgs e)
+        private void numberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             e.Handled = !int.TryParse(e.Text, out int result);
+        }
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if(sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableComboBox_TextChanged(object sender,TextChangedEventArgs e)
+        {
+            if(sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
         }
     }
 }
